17bda4a6668d37689ad5731e1bf5a4cad39f39cc
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2020-11-24 22:01:24 +0100

    test: made a prototype of the three concatenated and multiple requests that are needed to recover the japanase rrelated words

--- src/app/related-words-searcher/related-words-searcher.component.ts
+++ src/app/related-words-searcher/related-words-searcher.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RelatedWord } from '../related-word';
 import { SearchEnglishRelatedWordsService } from '../search-english-related-words.service';
 import { DatamuseWord } from '../datamuse-word';
+import { Observable, of, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-related-words-searcher',
@@ -12,6 +13,7 @@ export class RelatedWordsSearcherComponent implements OnInit {
   word: string = 'example';
   relatedWords: RelatedWord[] = [];
   datamuseWords: DatamuseWord[] = [];
+  varForTestingConcatenatedRequests: number[] = [];
 
   constructor(
     private searchEnglishRelatedWordsService: SearchEnglishRelatedWordsService
@@ -24,5 +26,31 @@ export class RelatedWordsSearcherComponent implements OnInit {
     this.searchEnglishRelatedWordsService
       .search(this.word)
       .subscribe((datamuseWords) => this.datamuseWords = datamuseWords);
+  }
+
+  firstRequest(): Observable<number[]> {
+    const myNumbers = [1, 2, 3, 4, 5];
+    return of(myNumbers)
+  }
+
+  secondRequest(myNumber: number): Observable<number> {
+    myNumber += 1
+    return of(myNumber)
+  }
+
+  thirdRequest(myNumber: number): Observable<number> {
+    myNumber += 2
+    return of(myNumber)
+  }
+
+  onTestConcatenatedRequests(): void {
+    console.log('testing concatenated request')
+    this.firstRequest().subscribe(myNumbers => {
+      const myProcessedNumbers = myNumbers.map(this.secondRequest)
+      forkJoin(myProcessedNumbers).subscribe(myNumbers => {
+        const myProcessedNumbers = myNumbers.map(this.thirdRequest)
+        forkJoin(myProcessedNumbers).subscribe(myNumbers => this.varForTestingConcatenatedRequests = myNumbers)
+      })
+    });
   }
 }
