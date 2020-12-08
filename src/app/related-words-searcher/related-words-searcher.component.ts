58573c3b6996ca3c45d8864e8c3cf876848466c0
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2020-12-08 13:46:45 +0100

    feat: reduced max number of words showed. Also some formatting

--- src/app/related-words-searcher/related-words-searcher.component.ts
+++ src/app/related-words-searcher/related-words-searcher.component.ts
@@ -1,17 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { RelatedWord } from '../related-word';
 import { SearchEnglishRelatedWordsService } from '../search-english-related-words.service';
-import { DatamuseWord } from '../datamuse-word';
+import { RelatedWord } from '../related-word';
 import { TranslateEnglishRelatedWordsService } from '../translate-english-related-words.service';
 import { TranslatedWord } from '../translated-word';
-import { Observable, throwError, of, forkJoin } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
-import {
-  HttpClient,
-  HttpParams,
-  HttpErrorResponse,
-} from '@angular/common/http';
-
+import { Observable, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-related-words-searcher',
@@ -21,36 +13,33 @@ import {
 export class RelatedWordsSearcherComponent implements OnInit {
   word: string = 'example';
   relatedWords: RelatedWord[] = [];
-  datamuseWords: DatamuseWord[] = [];
   translatedWords: TranslatedWord[] = [];
- 
-  varForTestingConcatenatedRequests: number[] = [];
 
-  googleTranslateUrl = 'https://translation.googleapis.com/language/translate/v2';
+  maxNumberOfWords = 20;
 
   constructor(
     private searchEnglishRelatedWordsService: SearchEnglishRelatedWordsService,
-    private translateEnglishRelatedWordsService: TranslateEnglishRelatedWordsService,
-    private http: HttpClient
+    private translateEnglishRelatedWordsService: TranslateEnglishRelatedWordsService
   ) {}
 
   ngOnInit(): void {}
 
   onSearch(): void {
-    console.log('Searching for ' + this.word);
     this.searchEnglishRelatedWordsService
       .search(this.word)
-      .subscribe(datamuseWords => {
-        this.datamuseWords = datamuseWords
+      .subscribe((relatedWords) => {
+        relatedWords = relatedWords.slice(0, this.maxNumberOfWords);
+        this.relatedWords = relatedWords;
         let translatedWordsObservables: Observable<TranslatedWord>[] = [];
-        for (const datamuseWord of datamuseWords) {
-          const translatedWordObservable = this.translateEnglishRelatedWordsService.translateWord(datamuseWord)
-          translatedWordsObservables.push(translatedWordObservable)
+        for (const relatedWord of relatedWords) {
+          const translatedWordObservable = this.translateEnglishRelatedWordsService.translateWord(
+            relatedWord
+          );
+          translatedWordsObservables.push(translatedWordObservable);
         }
-        forkJoin(translatedWordsObservables).subscribe(translatedWords => {
-          this.translatedWords = translatedWords
-        })  
+        forkJoin(translatedWordsObservables).subscribe((translatedWords) => {
+          this.translatedWords = translatedWords;
+        });
       });
   }
-
 }
