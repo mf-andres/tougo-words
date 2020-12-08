dfa5574c9aa862d333540d7400fb9a9d8d19db2c
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2020-12-08 14:16:57 +0100

    refactor: searcher component controller

--- src/app/related-words-searcher/related-words-searcher.component.ts
+++ src/app/related-words-searcher/related-words-searcher.component.ts
@@ -29,25 +29,53 @@ export class RelatedWordsSearcherComponent implements OnInit {
     this.searchEnglishRelatedWordsService
       .search(this.word)
       .subscribe((relatedWords) => {
-        relatedWords = relatedWords.slice(0, this.maxNumberOfWords);
-        this.relatedWords = relatedWords;
-        let translatedWordsObservables: Observable<TranslatedWord>[] = [];
-        for (const relatedWord of relatedWords) {
-          const translatedWordObservable = this.translateEnglishRelatedWordsService.translateWord(
-            relatedWord
-          );
-          translatedWordsObservables.push(translatedWordObservable);
-        }
-        forkJoin(translatedWordsObservables).subscribe((translatedWords) => {
-          this.translatedWords = translatedWords;
-          this.translatedWordsLinks = translatedWords.map(this.generateTranslatedWordLink)
-        });
+        this.setRelatedWords(relatedWords);
+        this.translateRelatedWordsAndSetMoreThings(relatedWords);
       });
   }
 
+  private setRelatedWords(relatedWords: RelatedWord[]) {
+    relatedWords = relatedWords.slice(0, this.maxNumberOfWords);
+    this.relatedWords = relatedWords;
+  }
+
+  translateRelatedWordsAndSetMoreThings(relatedWords: RelatedWord[]) {
+    const translatedWordsObservables = this.getTranslatedWordsObservables(
+      relatedWords
+    );
+    forkJoin(translatedWordsObservables).subscribe((translatedWords) => {
+      this.setTranslatedWords(translatedWords);
+      this.setTranslatedWordsLinks(translatedWords);
+    });
+  }
+
+  private getTranslatedWordsObservables(
+    relatedWords: RelatedWord[]
+  ): Observable<TranslatedWord>[] {
+    let translatedWordsObservables: Observable<TranslatedWord>[] = [];
+    for (const relatedWord of relatedWords) {
+      const translatedWordObservable = this.translateEnglishRelatedWordsService.translateWord(
+        relatedWord
+      );
+      translatedWordsObservables.push(translatedWordObservable);
+    }
+    return translatedWordsObservables;
+  }
+
+  private setTranslatedWords(translatedWords: TranslatedWord[]) {
+    this.translatedWords = translatedWords;
+  }
+
+  private setTranslatedWordsLinks(translatedWords: TranslatedWord[]) {
+    this.translatedWordsLinks = translatedWords.map(
+      this.generateTranslatedWordLink
+    );
+  }
+
   generateTranslatedWordLink(translatedWord: TranslatedWord): string {
-    const translatedWordText = translatedWord.data.translations[0].translatedText
-    const link = 'https://jisho.org/search/' + translatedWordText
-    return link
+    const translatedWordText =
+      translatedWord.data.translations[0].translatedText;
+    const link = 'https://jisho.org/search/' + translatedWordText;
+    return link;
   }
 }
