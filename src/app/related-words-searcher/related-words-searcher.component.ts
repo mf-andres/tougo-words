398606bde1ae1cdd5db3a9b37a18e55c8edf331d
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2020-12-08 13:33:36 +0100

    feat: related words are now translated

--- src/app/related-words-searcher/related-words-searcher.component.ts
+++ src/app/related-words-searcher/related-words-searcher.component.ts
@@ -42,51 +42,15 @@ export class RelatedWordsSearcherComponent implements OnInit {
       .search(this.word)
       .subscribe(datamuseWords => {
         this.datamuseWords = datamuseWords
-        // TODO Cambia este map por un for igual que abajo
-        const translatedWordsObservables = datamuseWords.map(this.translateEnglishRelatedWordsService.translateWord)
+        let translatedWordsObservables: Observable<TranslatedWord>[] = [];
+        for (const datamuseWord of datamuseWords) {
+          const translatedWordObservable = this.translateEnglishRelatedWordsService.translateWord(datamuseWord)
+          translatedWordsObservables.push(translatedWordObservable)
+        }
         forkJoin(translatedWordsObservables).subscribe(translatedWords => {
           this.translatedWords = translatedWords
         })  
       });
-  }
-
-  onTestTranslation(): void {
-    console.log('testing translations')
-    const relatedWord: RelatedWord = {word: "table"};
-    const relatedWords: RelatedWord[] = [relatedWord, relatedWord, relatedWord]
-    let translatedWordsObservables: Observable<TranslatedWord>[] = [];
-    // Cambia el bucle for de forma que invoque al servicio en vez de a las funciones de abajo
-    // Retoca también los imports
-    // Cambiar en la función principal la forma el map por el for
-    for (const relatedWord of relatedWords) {
-      const translatedWordObservable = this.translateWord(relatedWord)
-      translatedWordsObservables.push(translatedWordObservable)
-    }
-    console.log("observables constructed")
-    forkJoin(translatedWordsObservables).subscribe(translatedWords => {
-      this.translatedWords = translatedWords
-    })  
-  }
-
-  // Para depurar la traducción traje estas dos funciones a este componente
-  // Hay que probar a cambiar el bucle for de arriba de forma que invoque al servicio
-  translateWord(relatedWord: RelatedWord): Observable<TranslatedWord> {
-    const translationRequest = {q: relatedWord.word, source: "en", target: "ja", format: "text"}
-    const options = { params: new HttpParams().set('key', 'AIzaSyBI7M---IiAh89dcEbLxbJVFcwFJ3i5GvA') };
-    return this.http
-      .post<TranslatedWord>(this.googleTranslateUrl, translationRequest, options)
-      .pipe(catchError(this.handleError));
-  }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      console.error('An error occurred:', error.error.message);
-    } else {
-      console.error(
-        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-      );
-    }
-    return throwError('Something bad happened; please try again later.');
   }
 
 }
